66d4142191c162b2cf722ce679921485241eeb9e
hjlim <hjlim@hecto.cokr>
2025-09-09 09:59:37 +0900

    db수정

--- src/main/java/com/rbox/auth/adapter/in/web/LoginReq.java
+++ src/main/java/com/rbox/auth/adapter/in/web/LoginReq.java
@@ -8,6 +8,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "로그인 요청")
 public record LoginReq(
-        @Schema(description = "사용자 이메일", example = "user@example.com") @Email @NotBlank String email,
-        @Schema(description = "비밀번호", example = "Pa55w0rd!") @NotBlank @Size(min = 8, max = 128) String password
+        @Schema(description = "사용자 이메일", example = "lim8499@naver.com") @Email @NotBlank String email,
+        @Schema(description = "비밀번호", example = "a1231231") @NotBlank @Size(min = 8, max = 128) String password
 ) {}
